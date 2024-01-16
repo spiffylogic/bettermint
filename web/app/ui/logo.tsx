5e43a72a4b8abafff28010cb7594751b01c7ddd8
Mark <spiffylogic@gmail.com>
2024-01-15 20:01:04 -0500

    Start introducing side-nav component

--- web/app/ui/logo.tsx
+++ web/app/ui/logo.tsx
@@ -5,10 +5,8 @@ import Image from 'next/image';
 export default function Logo() {
   return (
     <div className={`${lusitana.className} flex flex-row items-center leading-none text-white`}>
-        <Link href="/">
-            <Image src="/mint.svg"  width={128} height={128} alt="Logo" />
-        </Link>
-        <p className="text-[44px]">Bettermint</p>
+        <Image src="/mint.svg"  width={128} height={128} alt="Logo" />
+        <p className="text-[32px]">Bettermint</p>
     </div>
   );
 }
